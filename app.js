18059a51572021acf04f2fe0b0b752a2755462a1
DrunkenLee <michaelsinx.one@gmail.com>
2023-09-09 13:58:41 +0700

    routes and appjs

---
+++ app.js
@@ -0,0 +1,9 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+const router = require('./Routes');
